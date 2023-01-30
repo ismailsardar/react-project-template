1d8b7ff0e4346d17ba316bf70412e380e635dfb2
ismail <ismailsardar@gmail.com>
2023-01-30 12:03:03 +0600

    React project Templect setap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/*eslint-disable prettier/prettier */
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+    </div>
+  );
+};
+
+export default App;
